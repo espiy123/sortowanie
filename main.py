7a520bdf40f80da1e862d1f64ab9915af668d9fd
Filip Pieprzyk <filip.pieprzyk@gmail.com>
2023-03-09 20:02:48 +0100

    rev 1.1

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from random import randrange
 
-#tworzenie listy z losowymi wartościami
+#tworzenie listy z losowymi wartosciami
 lista = []
 i = 0
 while i<100:
     lista.append(randrange(0,100))
     i += 1
 
-print(lista)
+#sortowanie bablekowe
+def sortowanie_babelkowe():
+    for j in range(len(lista)-1):
+        for i in range(len(lista)-1):
+            if lista[i]>lista[i+1]:
+                lista[i], lista[i+1] = lista[i+1], lista[i]
+            else:
+                continue
+    return lista
+
+print(sortowanie_babelkowe())
