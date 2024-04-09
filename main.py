a12f541835291bd8ee6435adacec75fced909aa7
Filip Pieprzyk <filip.pieprzyk@gmail.com>
2024-04-09 22:11:38 +0200

    final version

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import random
 import time
-import matplotlib.pyplot as plt
-import numpy as np
+import csv
 import sortingMethods as srt
 
-iloscTestow = 10
-dlugosc = 1000
+iloscTestow = 20
 dolnyKres = 0
-gornyKres = 10000
-sorting = srt.insertionSort()
+gornyKres = 30000
+sorting = srt.heapSort
 
 
 def vTableFuncion(table):
@@ -22,53 +20,49 @@ def vTableFuncion(table):
     table1 = table1[::-1]
     vShape = table1 + table2
     return vShape
-
-
-def vTest(tabela):
-    czasy = []
-    for i in range(1, dlugosc):
-        listaTestowa = tabela.copy()
-        listaTestowa = vTableFuncion(listaTestowa[0:i])
-        time1 = time.perf_counter()
-        for _ in range(iloscTestow):
-            sorting(listaTestowa[0:i])
-        time2 = time.perf_counter()
-        czasy.append((time2 - time1) / iloscTestow)
-    return czasy
-
-
 def test(lista):
-    czasy = []
-    for i in range(1, dlugosc):
-        temp = lista[0:i]
-        timeStart = time.perf_counter()
-        for y in range(iloscTestow):
-            sorting(temp)
-        timeStop = time.perf_counter()
-        czasy.append((timeStop - timeStart) / iloscTestow)
-    return czasy
+    startTime = time.perf_counter()
+    for i in range(iloscTestow):
+        sorting(lista)
+    stopTime = time.perf_counter()
+    return (stopTime-startTime)/iloscTestow
 
 
-masterTable = []
-for x in range(dlugosc):
-    masterTable.append(random.randint(dolnyKres, gornyKres))
-ascendingTable = sorted(masterTable)
-descendingTable = ascendingTable[::-1]
-cTable = [1 for _ in range(dlugosc)]
+wynikiAscending = []
 
-malejaca = np.array(test(descendingTable))
-rosnaca = np.array(test(ascendingTable))
-losowa = np.array(test(masterTable))
-vLista = np.array(vTest(ascendingTable))
-cLista = np.array(test(cTable))
+wynikiDescending = []
 
-plt.plot(malejaca, label='malejący')
-plt.plot(rosnaca, label='rosnący')
-plt.plot(losowa, label='losowa')
-plt.plot(vLista, label='v-kształtne')
-plt.plot(cLista, label='stała')
+wynikiCTable = []
 
-plt.xticks(np.arange(0, dlugosc, step=(dlugosc // 15)))
-plt.legend()
-plt.show()
-plt.savefig('test.png')
+wynikiVTable = []
+
+wynikiRandom = []
+
+indeksy = []
+naglowki = ['Rosnacy', "Malejacy", "Staly", "V-kształtny", "Losowy"]
+for x in range(2000,30001,2000):
+    lista = []
+    for _ in range(x):
+        lista.append(random.randint(dolnyKres,gornyKres))
+
+
+    ascending = sorted(lista)
+    descending = ascending[::-1]
+    cTable = [1 for _ in range(x)]
+    vTable = vTableFuncion(ascending)
+
+    wynikiRandom.append(test(lista))
+    wynikiAscending.append(test(ascending))
+    wynikiDescending.append(test(descending))
+    wynikiCTable.append(test(cTable))
+    wynikiVTable.append(test(vTable))
+
+    indeksy.append(x)
+
+with open("wyniki.csv", mode='w', newline='') as plik_csv:
+    writer = csv.writer(plik_csv)
+    writer.writerow(['Rozmiar'] + naglowki)
+    for indeks, asc, desc, ctable, vtable, rand in zip(indeksy, wynikiAscending, wynikiDescending, wynikiCTable, wynikiVTable, wynikiRandom):
+        writer.writerow([indeks, asc, desc, ctable, vtable, rand])
+
+
